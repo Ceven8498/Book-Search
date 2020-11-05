87bc16b0334030d943f2d7db1239f990493e3760
CEven98 <doomfire98@gmail.com>
2020-11-05 00:28:14 -0600

    finishing up

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/react-hooks';
 import { LOGIN_USER } from '../utils/mutations';
-//import { loginUser } from '../utils/API'; dont need anymore
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
